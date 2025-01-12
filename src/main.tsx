327ec72a18ff27d44a09d8b0525668aacf1a9b7a
Luis Alfonso <luisalfonsogp@gmail.com>
2025-01-12 21:20:53 +0100

    feat(config): updated project configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import GifExpertApp from './GifExpertApp.tsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import GifExpertApp from './GifExpertApp.tsx'
 
-import './styles.css'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <GifExpertApp />
-  </StrictMode>
+  </StrictMode>,
 )
